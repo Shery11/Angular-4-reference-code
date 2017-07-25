c60be276aefce6e53a916520e654d3381bbbb8d6
Shery11 <laughnplay>
2017-07-25 19:01:38 +0500

    initializing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angular app';
+
+  obj = {
+  	id : 123,
+  	name : "Shaharyar"
+  }
+
+  arr = [1,2,3,4,2,1,3];
+
+  isTrue = true;
+
+  myName = "Shery"
+
+  day = new Date(1992,3,6);
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    OneComponent,
+    TwoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+     {    
+       path:'one',
+       component: OneComponent
+      },
+      {    
+       path:'two',
+       component: TwoComponent
+      }
+
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
